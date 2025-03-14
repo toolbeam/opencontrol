28dc72d415332f9b354581fdac2025658a4a33ba
Dax Raad <d@ironbay.co>
2025-03-14 02:04:53 -0400

    stupid cookie

--- packages/opencontrol/src/index.ts
+++ packages/opencontrol/src/index.ts
@@ -4,6 +4,7 @@ import { createMcp } from "./mcp.js"
 import { basicAuth } from "hono/basic-auth"
 import { cors } from "hono/cors"
 import HTML from "opencontrol-frontend/dist/index.html" with { type: "text" }
+import { setCookie, getCookie } from "hono/cookie"
 
 export interface OpenControlOptions {
   key?: string
@@ -25,14 +26,20 @@ export function create(input: {
         credentials: false,
       }),
     )
-    .use(
-      input.key
-        ? basicAuth({
-            username: "opencontrol",
-            password: input.key || "password",
-          })
-        : undefined,
-    )
+    .get("/login/:key", (c) => {
+      setCookie(c, "key", c.req.param("key"), {
+        path: "/",
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30,
+      })
+      return c.redirect("/")
+    })
+    .use(async (c, next) => {
+      const key = getCookie(c, "key")
+      if (key !== input.key) return c.text("login at /login/:key")
+      return next()
+    })
     .get("/", (c) => {
       if (!input.anthropicApiKey)
         return c.text("Please set the anthropicApiKey to use built in chat")

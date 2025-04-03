4b3705d02b72c1d5e732a3ce9fac56090c55e055
Dax Raad <d@ironbay.co>
2025-04-03 18:35:44 -0400

    sync

--- examples/bun/index.ts
+++ examples/bun/index.ts
@@ -1,33 +1,33 @@
 import { create } from "opencontrol"
 import { tool } from "opencontrol/tool"
 import { z } from "zod"
-import AWS from "aws-sdk"
+import * as AWS from "aws-sdk"
 import { anthropic } from "@ai-sdk/anthropic"
+import { google } from "@ai-sdk/google"
 
 const aws = tool({
   name: "aws",
   description: "Make a call to the AWS SDK for JavaScript v2",
   args: z.object({
     client: z.string().describe("Class name of the client to use"),
-    command: z.string().describe("Command to call on the client"),
-    args: z
-      .record(z.string(), z.any())
-      .optional()
-      .describe("Arguments to pass to the command as json"),
+    command: z.string().describe("Function to call on the AWS sdk client"),
+    params: z.string().describe("Arguments to pass to the command as JSON"),
   }),
   async run(input) {
     // @ts-ignore
     const client = AWS[input.client]
     if (!client) throw new Error(`Client ${input.client} not found`)
     const instance = new client()
-    return await instance[input.command](input.args).promise()
+    const cmd = instance[input.command]
+    if (!cmd) throw new Error(`Command ${input.command} not found`)
+    return await cmd(JSON.parse(input.params)).promise()
   },
 })
 
 export default create({
   tools: [aws],
-  model: anthropic("claude-3-7-sonnet-latest"),
+  // model: anthropic("claude-3-7-sonnet-latest"),
   password: "password",
-  // model: google("gemini-2.0-flash-exp"),
+  model: google("gemini-2.5-pro-exp-03-25"),
   // model: openai("o3-mini"),
 })

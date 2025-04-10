c3763ff68dea03ec24a7637c9e5faa2c244947c6
Jay V <air@live.ca>
2025-04-10 17:43:47 -0400

    tweak lander

--- app/web/src/pages/index.tsx
+++ app/web/src/pages/index.tsx
@@ -25,10 +25,10 @@ export default function Index() {
 
             <section data-slot="cta">
               <div data-slot="col-1">
-                <a href={`${import.meta.env.VITE_DOCS_URL}/docs/`}>Docs</a>
+                <a href={`${import.meta.env.VITE_DOCS_URL}/docs/`}>Self-host</a>
               </div>
               <div data-slot="col-2">
-                <span onClick={() => auth.authorize({ provider: "google" })}>Try it</span>
+                <span onClick={() => auth.authorize({ provider: "google" })}>Sign in</span>
               </div>
               <div data-slot="col-3">
                 <a target="_blank" href="https://github.com/sst/opencontrol">
@@ -37,36 +37,30 @@ export default function Index() {
               </div>
             </section>
 
+            <section data-slot="content">
+              <p>
+                OpenControl lets you control your infrastructure with AI. You can use
+                it via:
+              </p>
+              <ol>
+                <li>
+                  <b>OpenControl.ai</b>: Hosted on our side using our LLMs. You can
+                  connect your AWS account and other services.
+                </li>
+                <li>
+                  <b>Self-host</b>: Runs in your infrastructure with access to
+                  your internal resources using your LLMs.
+                </li>
+              </ol>
+            </section>
+
             <section data-slot="images">
               <div>
-                <p>Architecture</p>
+                <p>Self-hosted Architecture</p>
                 <a href={`${import.meta.env.VITE_DOCS_URL}/docs/how-it-works/`}>
                   <ImageArchitecture width="100%" />
                 </a>
               </div>
-            </section>
-
-            <section data-slot="content">
-              <ul>
-                <li>
-                  <b>Self-hosted</b>: Runs in your infrastructure with access to
-                  internal resources and functions from your codebase; deploys
-                  to AWS Lambda, Cloudflare Workers, or containers.
-                </li>
-                <li>
-                  <b>Unified gateway</b>: Generates a single HTTP endpoint that
-                  you can chat with or register with your AI client and it
-                  exposes all your tools.
-                </li>
-                <li>
-                  <b>Universal</b>: Works with any model that supports tool
-                  calling. Like the models from Anthropic, OpenAI, or Google.
-                </li>
-                <li>
-                  <b>Secure</b>: Supports authentication through any OAuth
-                  provider.
-                </li>
-              </ul>
             </section>
 
             <section data-slot="footer">
